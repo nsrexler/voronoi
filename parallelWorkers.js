5d897aa74ca84e1e9eebf75411308d610aeda470
nsrexler <39609919+nsrexler@users.noreply.github.com>
2024-05-23 17:32:01 -0700

    Interlace pixel workers

--- parallelWorkers.js
+++ parallelWorkers.js
@@ -1,5 +1,11 @@
 "use strict";
 
+Object.defineProperty(Function.prototype, "params", {
+    get() {
+        return this.toString().match(/\(([^\)]+)\)/)[1];
+    }
+});
+
 function initWorkers(workerFn, fnsToInclude, constantsToInclude, workerParams) {
     fnsToInclude = [workerFn, ...(fnsToInclude ?? [])];
     constantsToInclude = constantsToInclude ?? {};
@@ -30,20 +36,33 @@ function initWorkers(workerFn, fnsToInclude, constantsToInclude, workerParams) {
 }
 
 function initPixelWorkers(pixelFn, fnsToInclude, constantsToInclude) {
-    fnsToInclude = [pixelFn, ...(fnsToInclude ?? [])];
     if(pixelFn.name !== "pixelFn") {
         constantsToInclude = constantsToInclude ?? {};
         constantsToInclude.pixelFn = pixelFn;
     }
-    const workerParams = pixelWorkerFn.params.replace("...childParams", pixelFn.params.split(",").slice(3).join(","));
-    return initWorkers(pixelWorkerFn, fnsToInclude, constantsToInclude, workerParams);
-}
-
-Object.defineProperty(Function.prototype, "params", {
-    get() {
-        return this.toString().match(/\(([^\)]+)\)/)[1];
+    else {
+        fnsToInclude = [pixelFn, ...(fnsToInclude ?? [])];
     }
-});
+    const workerParams = pixelWorkerFn.params.replace("...childParams", pixelFn.params.split(",").slice(3).join(","));
+    const workers = initWorkers(pixelWorkerFn, fnsToInclude, constantsToInclude, workerParams);
+    return async (width, height, workerParams) => {
+        const imgData = new ImageData(width, height);
+        const chunkSize = Math.ceil(height / workers.length);
+        await Promise.all(workers.map((worker, i) => {
+            const start = i * chunkSize;
+            const end = Math.min(start + chunkSize, height);
+            const startTime = new Date();
+            return worker.runAsync({ start, end, width, ...workerParams })
+                .then(colorData => {
+                    const offset = 4 * start * width;
+                    imgData.data.set(colorData, offset);
+                    const elapsed = (new Date()) - startTime;
+                    console.log(`Worker ${i}: ${elapsed / 1000}s`);
+                });
+        }));
+        return imgData;
+    }
+}
 
 function pixelWorkerFn(start, end, width, ...childParams) {
     const colorData = new Uint8ClampedArray(4 * (end - start) * width);
@@ -57,17 +76,45 @@ function pixelWorkerFn(start, end, width, ...childParams) {
     return colorData;
 }
 
-async function runPixelWorkers(workers, width, height, workerParams) {
-    const imgData = new ImageData(width, height);
-    const chunkSize = Math.ceil(height / workers.length);
-    await Promise.all(workers.map((worker, i) => {
-        const start = i * chunkSize;
-        const end = Math.min(start + chunkSize, height);
-        return worker.runAsync({ start, end, width, ...workerParams })
-            .then(colorData => {
-                const offset = 4 * start * width;
-                imgData.data.set(colorData, offset);
-            });
-    }));
-    return imgData;
+function initPixelWorkersInterlaced(pixelFn, fnsToInclude, constantsToInclude) {
+    if(pixelFn.name !== "pixelFn") {
+        constantsToInclude = constantsToInclude ?? {};
+        constantsToInclude.pixelFn = pixelFn;
+    }
+    else {
+        fnsToInclude = [pixelFn, ...(fnsToInclude ?? [])];
+    }
+    const workerParams = pixelWorkerFnInterlaced.params.replace("...childParams", pixelFn.params.split(",").slice(3).join(","));
+    const workers = initWorkers(pixelWorkerFnInterlaced, fnsToInclude, constantsToInclude, workerParams);
+    return async (width, height, workerParams) => {
+        const imgData = new ImageData(width, height);
+        const chunkSize = Math.ceil(height / workers.length);
+        await Promise.all(workers.map((worker, i) => {
+            const startTime = new Date();
+            return worker.runAsync({ offset: i, numWorkers: workers.length, height, width, ...workerParams })
+                .then(colorData => {
+                    for(let j = 0; j < colorData.length; j++) {
+                        const colorRow = colorData[j];
+                        imgData.data.set(colorRow, 4 * (i + j * workers.length) * width);
+                    }
+                    const elapsed = (new Date()) - startTime;
+                    console.log(`Worker ${i}: ${elapsed / 1000}s`);
+                });
+        }));
+        return imgData;
+    }
+}
+
+function pixelWorkerFnInterlaced(offset, numWorkers, height, width, ...childParams) {
+    //const colorData = new Uint8ClampedArray(4 * (Math.floor(height / chunkSize) + (offset < (height % chunkSize) ? 1 : 0)));
+    const colorData = [];
+    for(let y = offset; y < height; y += numWorkers) {
+        const colorRow = new Uint8ClampedArray(4 * width);
+        for(let x = 0; x < width; x++) {
+            const color = pixelFn(y, x, width, ...childParams);
+            colorRow.set(color, 4 * x);
+        }
+        colorData.push(colorRow);
+    }
+    return colorData;
 }
